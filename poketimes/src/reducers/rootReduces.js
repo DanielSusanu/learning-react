edd253cc151c0f6af769242c3298744d4f80dc9b
Daniel Susanu <d.susanu.development@gmail.com>
2018-12-22 01:51:11 +0000

    Added Redux and initialised redux store

---
+++ poketimes/src/reducers/rootReduces.js
@@ -0,0 +1,28 @@
+const initState = {
+ posts: [
+  {
+   id: 1,
+   title: "qui est esse",
+   body:
+    "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+   id: 2,
+   title: "eum et est occaecati",
+   body:
+    "est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+   id: 3,
+   title: "nesciunt quas odio",
+   body:
+    "repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque"
+  }
+ ]
+};
+
+const rootReducer = (state = initState, action) => {
+ return state;
+};
+
+export default rootReducer;
